7db5ffbbf1d6f308bd9a4d8b2f997c05b241a062
MagicTINTIN <valentinservieres@gmail.com>
2023-08-25 11:23:40 +0200

    adding config file structure

--- sort.py
+++ sort.py
@@ -1,8 +1,47 @@
-import os
+import os, json, datetime
 
 def mkdir(path):
     if not os.path.isdir(path):
         os.makedirs(path)
 
-mkdir("test")
-os.rename('test', 'hello world')
+# opens the previous config if there was one
+try:
+    with open('bands.json') as f:
+        bands = json.load(f)
+        print("Previous config found")
+
+        os.rename(
+            'bands.json', 
+            "bandsave" + datetime.datetime.strftime(datetime.datetime.now() ,"%Y-%m-%d_%H-%M-%S") + ".json"
+            )
+
+        categories = ["labels", "uploaders", "bandnames"]
+        for el in categories:
+
+            if not el in bands:
+                print("+ " + el + " -> config")
+                if el == "labels":
+                    bands[el] = []
+                else:
+                    bands[el] = {}
+
+except IOError:
+    print("No previous config found")
+    bands = {
+        # channels that upload musics which publish musics for multiple bands
+        "labels": [],
+        # name of uploaders that publish musics for only one band
+        "uploaders": {},
+        # list of uploaders by band name
+        "bandnames": {},
+    }
+
+# bands = json.dumps(jsonfilecfg)
+
+with open('bands.json', 'w') as f:
+    json.dump(bands, f)
+
+
+#mkdir("test")
+#os.rename('test', 'hello world')
+
