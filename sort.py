1fc5b10597e86c1982eb2306421b9f5490456867
MagicTINTIN <valentinservieres@gmail.com>
2023-08-25 14:38:16 +0200

    adding import argument to sort

--- sort.py
+++ sort.py
@@ -1,8 +1,43 @@
-import os, json, datetime
+import sys, os, json, datetime, pip
+
+try:
+    from unidecode import unidecode
+except ImportError:
+    print("Unidecode is not installed on this computer.\nPlease install it with pip install Unidecode (or with pacman -Sy python-unidecode)")
+    exit(1)
+
+yes = {'yes','y', 'ye', ''}
+no = {'no','n'}
+
+def addbanduploader(band, name="notaband", othername=False):
+    uploader = name if othername else band
+    if band in bands["bandnames"]:
+        bands["bandnames"][band].append(uploader)
+    else:
+        bands["bandnames"][band] = [ uploader ]
+    
+def addchannel(band, name="notaband", othername=False):
+    if othername:
+        bands["uploaders"][name] = band
+    else:
+        bands["uploaders"][band] = band
+    return addbanduploader(band, name, othername)
 
 def mkdir(path):
     if not os.path.isdir(path):
         os.makedirs(path)
+
+def importfile():
+    try:
+        with open('bandlist') as bandlistf:
+            for line in bandlistf:
+                bandname = unidecode(line.rstrip()).upper()
+                if len(bandname) > 0:
+                    addchannel(bandname)
+        return True
+    except IOError:
+        print("No bandlist found")
+        return False
 
 # opens the previous config if there was one
 try:
@@ -37,9 +72,25 @@ except IOError:
     }
 
 # bands = json.dumps(jsonfilecfg)
+if "--import" in sys.argv:
+    notanswered = True
+
+    while notanswered:
+        choice = input("Do you have a file containing bands name named ./bandlist ? (Y/n) ").lower()
+        if choice in yes:
+            notanswered = False
+            if importfile():
+                print("Band names successfully imported")
+            else:
+                print("Error while importing band names")
+        elif choice in no:
+            notanswered = False
+            print("Import cancelled")
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no'\n")
 
 with open('bands.json', 'w') as f:
-    json.dump(bands, f)
+    json.dump(bands, f, indent=4)
 
 
 #mkdir("test")
