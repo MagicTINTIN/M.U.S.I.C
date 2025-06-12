5e99d185f910c5e769f5f1b81b9ce31aeb78a13c
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-06-13 00:29:21 +0200

    fix: manually cleaning all the tags

--- tags2.py
+++ tags2.py
@@ -6,17 +6,19 @@ def tagMusic(music, artist, title):
         audiofile = OggOpus(music)
         audiofile["artist"] = artist
         audiofile["title"] = title
+        audiofile["genre"] = ["🇬🇷", "test", "music de fou"]
         audiofile.save()
         print(f"Tagged {music} with artist: {artist}, title: {title}")
     except Exception as e:
         print(f"Error tagging file {music}: {e}")
 
-path = input("Give the folder to tag (Enter a path ex: rawmusic/sorted) ")
-sortedFileList = sorted(os.listdir(path))
-print(path)
+# path = input("Give the folder to tag (Enter a path ex: rawmusic/sorted) ")
+# sortedFileList = sorted(os.listdir(path))
+# print(path)
 
-for d in sortedFileList:
-    if os.path.isdir(os.path.join(path,d)):
-        for f in sorted(os.listdir(os.path.join(path,d))):
+tagMusic("./test.opus", "moi", "c'est moi")
+# for d in sortedFileList:
+#     if os.path.isdir(os.path.join(path,d)):
+#         for f in sorted(os.listdir(os.path.join(path,d))):
             
-            tagMusic(os.path.join(path,d,f), d, f.replace(".opus", "").split('-', 1)[1].lstrip())
+#             tagMusic(os.path.join(path,d,f), d, f.replace(".opus", "").split('-', 1)[1].lstrip())
