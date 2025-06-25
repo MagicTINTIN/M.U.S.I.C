85167b9436d9dadda91cf908eebb6c78c0a48a84
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-06-25 18:56:54 +0200

    fix: youtube needs my cookies...

--- addToSorted.sh
+++ addToSorted.sh
@@ -1,6 +1,6 @@
 originalPath=`pwd`
 cd /run/media/user/Music/M.U.S.I.C/downloaded
-yt-dlp -i -R 5 --extract-audio --audio-format opus --audio-quality 0 --output "0þ%(uploader)sß%(title)s.%(ext)s" $1
+yt-dlp -i -R 5 --extract-audio --audio-format opus --audio-quality 0 --output "0þ%(uploader)sß%(title)s.%(ext)s" $1  --cookies-from-browser brave
 cd ../
 LASTFM_API_KEY="`cat ../k`"
 export LASTFM_API_KEY
